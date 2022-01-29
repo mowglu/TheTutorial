fb660a5ce68c341c4d8eb31351c99b53090208d6
Siddharth Raghavan <mowglu@gmail.com>
2022-01-29 11:04:01 -0500

    This is my first commit! Yay!

---
+++ main.py
@@ -0,0 +1,9 @@
+def main_wrapper():
+    # Attributes are a thing!
+    print(f"This is the start of our Python project. This function's name is {main_wrapper.__name__}")
+    # Code here
+
+    print("This is the end of our Python project.")
+
+if __name__ == "__main__":
+    main_wrapper()
