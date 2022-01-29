c3f39265fe571124f5004e3df13081979686c8e3
Siddharth Raghavan <mowglu@gmail.com>
2022-01-29 12:34:04 -0500

    Set up env variable, set up fetchers!

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+from api import fetcher
 
 def main_wrapper():
     # Attributes are a thing!
     print(f"This is the start of our Python project. This function's name is {main_wrapper.__name__}")
     # Code here
+    fetcher.states_accessor()
 
     print("This is the end of our Python project.")
 
