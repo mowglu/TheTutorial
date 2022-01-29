de7a3b782021b720fdbd039c2f7e345fe3ab3a47
Siddharth Raghavan <mowglu@gmail.com>
2022-01-29 13:22:12 -0500

    sixth commit

--- project/main.py
+++ project/main.py
@@ -1,4 +1,5 @@
 from api import fetcher
+from data import aggregate
 
 
 # Let's start in main.py showing some wrapper patterns and depicting method attributes.
@@ -23,7 +24,8 @@ def main_wrapper():
 
     #4. Finish API fetcher. Also add some data folders in gitignore. Like data_to_ignore (with sample example) Also utils example
 
-    #5.
+    #5. Fixed dataset
+    flight_list_fixed = aggregate.fixed_dataset()
 
     print("This is the end of our python project")
 
